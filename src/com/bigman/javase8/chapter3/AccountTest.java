bb9e46bf0134e162394b90a60d48e761e0022a65
Anthony McClay <anthony.mcclay@nielsen.com>
2016-01-01 09:44:43 -0500

    Added My Account File

--- src/com/bigman/javase8/chapter3/AccountTest.java
+++ src/com/bigman/javase8/chapter3/AccountTest.java
@@ -25,7 +25,7 @@ public class AccountTest {
 
         myAccount.setName(accName);
 
-        System.out.printf("New account name %s%n%n", myAccount.getName());
+        System.out.printf("New account base on myAccount name %s%n%n", myAccount.getName());
 
 
     }
