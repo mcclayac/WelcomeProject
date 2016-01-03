a3aee21bdb4d230a734b17b25993cbb4a7d5769d
Anthony McClay <anthony.mcclay@nielsen.com>
2016-01-03 12:44:45 -0500

    Added My Account File

---
+++ src/com/bigman/javase8/chapter7/ArrayDeclaration.java
@@ -0,0 +1,21 @@
+package com.bigman.javase8.chapter7;
+
+/**
+ * Created by anthonymcclay on 1/3/16.
+ */
+public class ArrayDeclaration {
+
+
+
+    public static void main(String[] args) {
+
+        int [] c = new int[12];
+
+        int [] myIntArray;
+        myIntArray = new int[12];
+
+        String[] b = new String[100];
+        String[] e = new String[30];
+
+    }
+}
